7e1331c3fc0727626d6a62ac612c6bacb44c2719
Gabriel.Alves <gabrieljorge343@gmail.com>
2022-08-27 11:06:30 -0300

    feat: Folow the car

--- src/marlin_simulator/scripts/aprox_objetivo.py
+++ src/marlin_simulator/scripts/aprox_objetivo.py
@@ -1,56 +1,47 @@
 #!/usr/bin/env python3
 
 import rospy
-from geometry_msgs.msg import PointStamped
-from std_msgs.msg import Float64
-import math
+from geometry_msgs.msg import PointStamped, Point
+
 
 class ModularVelocidade():
     def __init__(self):
-
+        self.tempo = 0.0
         rospy.init_node('mediar_orientacao', anonymous=True)
         rospy.Subscriber('sonar_data', PointStamped, self.callback)
-        self.objetivo_x = rospy.Publisher('objetivo_X', Float64, queue_size=10)
-        self.objetivo_y = rospy.Publisher('objetivo_Y', Float64, queue_size=10)
-
+        self.velocidade = rospy.Publisher('objetivo', Point, queue_size=10)
+            
+            
     def callback(self, posicao_relativa):
-        
-        distancia_x = posicao_relativa.point.x
-        distancia_y =  posicao_relativa.point.y
-        distancia_z = posicao_relativa.point.z
-        
-        mover_lado = Float64()
-        mover_frente = Float64()
-        
-        if distancia_x > 2:
-            mover_lado.data = -3.0
+            rate = rospy.Rate(100)
+            tempo2 = rospy.Time.now().to_sec() - self.tempo   
+            self.tempo = rospy.Time.now().to_sec()
+            distancia_x = posicao_relativa.point.x
+            distancia_y =  posicao_relativa.point.y
+            distancia_z = posicao_relativa.point.z #vai ser sempre 0
+            
+            mover = Point()
+            
+            if distancia_x >= 1.5 or distancia_x <= -1.5:
+                velocidade_x = -distancia_x/tempo2
+            else:
+                velocidade_x = 0.0
 
-        elif  distancia_x < -2 :
-            mover_lado.data = 3.0
-        
-        else: 
-            mover_lado.data = 0.0
-
-        if distancia_y > 2:
-            mover_frente.data = -3.0
-
-        elif distancia_y < -2:
-            mover_frente.data = 3.0
-
-        else: 
-            mover_frente.data = 0.0
-        
-        self.objetivo_x.publish(mover_lado)
-        self.objetivo_y.publish(mover_frente)
-        rospy.loginfo("Frente")
-        rospy.loginfo(mover_frente)
-        rospy.loginfo(distancia_y)
-        rospy.loginfo("Lado")
-        rospy.loginfo(mover_lado)
-        rospy.loginfo(distancia_x)
+            if distancia_y >= 1.5 or distancia_y <= -1.5:
+                velocidade_y = -distancia_y/tempo2
+            else: 
+                velocidade_y = 0.0
+            
+            mover.x = velocidade_x
+            mover.y = velocidade_y
+            mover.z = 0.0
+            print(mover)
+            
+            self.velocidade.publish(mover)
+            rate.sleep()
 
 
 if __name__ == '__main__':
-    while not rospy.is_shutdown():
-        l = ModularVelocidade()
-        # rospy.spin()
+    
+    l = ModularVelocidade()
+    rospy.spin()
