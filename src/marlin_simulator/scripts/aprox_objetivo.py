432b301e099a339880162e9601b7e26919592847
jpbtavaress <jpbt2001@poli.ufrj.br>
2022-08-28 15:00:42 -0300

    Conclusão do Projeto com scripts comentado

--- src/marlin_simulator/scripts/aprox_objetivo.py
+++ src/marlin_simulator/scripts/aprox_objetivo.py
@@ -11,37 +11,25 @@ class Orientation():
     def __init__(self):
         self.tetaCam = 0
         self.tetaObj = 0
-        rospy.Subscriber('sonar_data', PointStamped, self.callback)
-        rospy.Subscriber('odom', Odometry, self.callback2)
+        rospy.Subscriber('sonar_data', PointStamped, self.callback) #pegando as posições relativas entre os dois caminhos
+        rospy.Subscriber('odom', Odometry, self.callback2) #pegando as posições relativas dos carrinhos em relação ao mundo de forma vetorial
         self.diferenca_angular = rospy.Publisher('diferenca_angular', Float64, queue_size=10)
         
-            
-    def callback2(self, posicao_absoluta):
-        x = posicao_absoluta.pose.pose.orientation.x 
-        y = posicao_absoluta.pose.pose.orientation.y
-        z = posicao_absoluta.pose.pose.orientation.z
-        w = posicao_absoluta.pose.pose.orientation.w
-        yaw_z = self.euler_from_quaternion( x, y, z, w)
-        
-        self.tetaCam = yaw_z
-        self.diferenca_angular.publish(self.tetaCam-self.tetaObj)
-        
-
     def callback(self, posicao_relativa):
-            x1 = posicao_relativa.point.x
-            y1 =  posicao_relativa.point.y
+        x1 = posicao_relativa.point.x #instanciando a posição que o tópico "sonar_data" nos forneceu no 1° subscriber
+        y1 =  posicao_relativa.point.y
 
-            x2=0
-            y2=-1
+        x2=0 #definindo um vetor unitário no eixo y que servirá de referência para o angulo entre os carrinhos
+        y2=-1
 
-            if x1 > 0:
-                teta = math.acos(x1*x2 + y1*y2 / (math.sqrt(x1**2 + y1**2) * math.sqrt( x2**2 + y2**2)))
-            else:
-                teta = -math.acos(x1*x2 + y1*y2 / (math.sqrt(x1**2 + y1**2) * math.sqrt( x2**2 + y2**2)))
+        if x1 > 0: #descobrindo o angulo entre os dois vetores pelo produto interno.
+            teta = math.acos(x1*x2 + y1*y2 / (math.sqrt(x1**2 + y1**2) * math.sqrt( x2**2 + y2**2)))
+        else:
+            teta = -math.acos(x1*x2 + y1*y2 / (math.sqrt(x1**2 + y1**2) * math.sqrt( x2**2 + y2**2)))
 
-            self.tetaObj = teta
-    
-    def euler_from_quaternion(self, x, y, z, w):
+        self.tetaObj = teta #instanciando o angulo entre a normal(eixo de referência) e o vetor do nemo e marlin
+        
+    def euler_from_quaternion(self, x, y, z, w): #transformar o quartenium em um ângulo (euler)
         """
         Convert a quaternion into euler angles (roll, pitch, yaw)
         roll is rotation around x in radians (counterclockwise)
@@ -49,10 +37,27 @@ class Orientation():
         yaw is rotation around z in radians (counterclockwise)
         """
         # https://automaticaddison.com/how-to-convert-a-quaternion-into-euler-angles-in-python/
+        # Link da origem do código
+        
         t3 = +2.0 * (w * z + x * y)
         t4 = +1.0 - 2.0 * (y * y + z * z)
         yaw_z = math.atan2(t3, t4)
-        return  yaw_z 
+        return  yaw_z  #só retornamos a rotação ao redor de z(em radianos), já que nosso carrinho não rotaciona nas demais direções
+        
+    def callback2(self, posicao_absoluta): #descobre o angulo da câmera do marlin em relação ao eixo y do mundo
+        x = posicao_absoluta.pose.pose.orientation.x #instanciando as infos vindas do tópico odom
+        y = posicao_absoluta.pose.pose.orientation.y
+        z = posicao_absoluta.pose.pose.orientation.z
+        w = posicao_absoluta.pose.pose.orientation.w
+        yaw_z = self.euler_from_quaternion( x, y, z, w) #guarda o angulo de rotação da função em torno de Z
+        
+        self.tetaCam = yaw_z
+        
+        #Definimos então a diferença angular entre o ângulo entre os carrinhos e o eixo y e
+        #o angulo da câmera e o eixo Y. Portanto o ângulo que devemos  girar o marlin para 
+        #seguir a trajetória do nemo corretamente.
+        self.diferenca_angular.publish(self.tetaCam-self.tetaObj) #publicando no tópico diferença angular
+        
             
 
 if __name__ == '__main__':
--- src/marlin_simulator/scripts/marlin_move.py
+++ src/marlin_simulator/scripts/marlin_move.py
@@ -10,14 +10,16 @@ class Movement():
     def __init__(self):
         self.tempo = 0.0
         self.velocidade_angular = 0.0
-        self.velocidade = 0.0
-
-        rospy.Subscriber('diferenca_angular', Float64, self.girar)
+        self.velocidade = 0.0 #vai ser atualizada sempre que a função acelerar rodar,
+                              #ou seja, a cada vez que o sonar_data publica alguma coisa
+        rospy.Subscriber('diferenca_angular', Float64, self.girar) #pegou o ângulo do tópico em que publicamos a diferença angular
         rospy.Subscriber('sonar_data', PointStamped, self.acelerar)
-        
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size = 10) 
-            
-    def girar(self, diferenca_angular):
+
+    def girar(self, diferenca_angular): 
+        # "diferenca_angular" vem em formato de radiano (angulo em graus × π/180)
+        #6 rad = 343°
+        #3 rad = 171.8° ...
         
         if  abs(diferenca_angular.data) < 6 and abs(diferenca_angular.data) >= 3.0 or diferenca_angular.data < 0:
             self.velocidade_angular = 6.0
@@ -28,38 +30,53 @@ class Movement():
 
         mover = Twist()
         mover.linear.x = 0.0
-        mover.linear.y = self.velocidade
+        mover.linear.y = self.velocidade #instanciamos aqui a velocidade calculada na função "acelerar".
+        #A razão de publicarmos a velocidade junto com o tópico "velocidade angular", que utiliza o
+        #rate do tópico "odom" foi para não termos um atraso do carrinho em razão do rate lento
+        #do "sonar_data". Dessa forma, toda hora que o carrinho recebe uma informação de mudar sua 
+        #velocidade angular, publicamos novamente sua velocidade_linear (ainda que desatualizada, em razão
+        # do delay da função acelerar) para que ele não fique parado e depois aumente muito sua 
+        # velocidade ou parar de repente em virtude de uma desaleração do nemo.
         mover.linear.z = 0.0
         mover.angular.x = 0.0
         mover.angular.y = 0.0
         mover.angular.z = self.velocidade_angular
+        # Definimos a velocidade linear nos eixos x e z como zero para que o carrinho só ande paralelo a direção da roda do carrinho
+        #Defimos a velocidade angular nos eixos x e y como zero para que o carrinho muda sua direção pelo eixo z somente. (carrinho esta apoiado no chão, não faria sentido girar em torno do mesmo)
         rospy.loginfo(mover)
         rospy.loginfo(diferenca_angular.data)
 
         self.pub.publish(mover)
         
 
-    def acelerar(self, posicao_relativa):
+    def acelerar(self, posicao_relativa): #utilizamos as informações do sonar_data para calcularmos a velocidade.
+        
+        intervalo = rospy.Time.now().to_sec() - self.tempo 
+        self.tempo = rospy.Time.now().to_sec()
+        #Através desse trecho a cima, definimos o deltaT que o nemo se move em relação ao marlin
+        
+
+        distancia_x = posicao_relativa.point.x
+        distancia_y =  posicao_relativa.point.y
+        distancia_z = posicao_relativa.point.z 
+        #Dessa forma, dividino a distância entre os dois carrinhos pelo temo intervalo de tempo
+        #em que esse distanciamento ocorreu, definindo então a velocidade que o marlin deverá ter
+        #para seguir a trajetória do nemo de forma eficaz.
+        
+        if distancia_x >= 2.5 or distancia_x <= -2.5:
+            velocidade_x = -distancia_x/intervalo 
+        else:
+            velocidade_x = 0.0
+
+        if distancia_y >= 2.5 or distancia_y <= -2.5:
+            velocidade_y = -distancia_y/intervalo
+        else: 
+            velocidade_y = 0.0
+
             
-            intervalo = rospy.Time.now().to_sec() - self.tempo   
-            self.tempo = rospy.Time.now().to_sec()
-
-            distancia_x = posicao_relativa.point.x
-            distancia_y =  posicao_relativa.point.y
-            distancia_z = posicao_relativa.point.z 
-            
-            if distancia_x >= 2.5 or distancia_x <= -2.5:
-                velocidade_x = -distancia_x/intervalo 
-            else:
-                velocidade_x = 0.0
-
-            if distancia_y >= 2.5 or distancia_y <= -2.5:
-                velocidade_y = -distancia_y/intervalo
-            else: 
-                velocidade_y = 0.0
-
-             
-            self.velocidade = sqrt(velocidade_x**2+velocidade_y**2) 
+        self.velocidade = sqrt(velocidade_x**2+velocidade_y**2) 
+        # Como o carrinho so se move na direção y, paralela a orientação de suas rodas, calculamos 
+        # o módulo  das velocidades em cada eixo e publicamos como uma única velocidade.
 
             
 
